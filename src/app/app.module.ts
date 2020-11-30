cfd3877c4ba7e918a2f2c022c124a6ce943371ea
jaywardhan1010 <jaywardhan.ivision@gmail.com>
2020-11-30 18:30:09 +0530

    git first try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CollaborationComponent } from './collaboration/collaboration.component';
+import { ServicesComponent } from './services/services.component';
+import { CapabilitiesComponent } from './capabilities/capabilities.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CollaborationComponent,
+    ServicesComponent,
+    CapabilitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
